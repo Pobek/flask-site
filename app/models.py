6ec55de647453d931c19bda92fa3aa6846f5ba28
Sputters <coevyatar@gmail.com>
2018-08-04 15:52:20 +0300

    added follower option

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from flask_login import UserMixin
 from hashlib import md5
 from app import db, login
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -15,6 +20,11 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
@@ -25,6 +35,17 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- blob.py
+++ blob.py
@@ -1,8 +1,9 @@
 from app import app, db
 from app.models import User, Post
 
-app.run(host='0.0.0.0')
-
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
